c1677d0571af763880d75732558cc1ea2d086d25
Sara Bastian <seb118@georgetown.edu>
2020-09-14 02:34:17 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -3,11 +3,13 @@
 require 'pry'
 
 def generate_star_date
+  binding.pry
   (rand(100000) + 400000) / 10.0
 end
 
 
 def state_log(star_date)
+  binding.pry
   "Captain's Log, star date #{star_date}."
 end
 
