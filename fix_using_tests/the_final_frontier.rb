c0a56f55ad1c6a45e17b4f166c4a2b8625efd885
Sara Bastian <seb118@georgetown.edu>
2020-09-14 02:36:12 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -3,21 +3,16 @@
 require 'pry'
 
 def generate_star_date
-  binding.pry
   (rand(100000) + 400000) / 10.0
 end
 
 
 def state_log(star_date)
-  binding.pry
   "Captain's Log, star date #{star_date}."
 end
 
 def engage
   
-  puts state_log(date)
-  
+  puts state_log(star_date)
   date = generate_star_date
-  binding.pry
-  puts 'hello'
 end
